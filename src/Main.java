4b9f3f721e61330ce39c1d482f922b123a3da223
aynazmeymanat <aynazmeymanat@gmail.com>
2024-04-22 12:43:34 +0330

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("choose one of the following item");
+        System.out.println("1:sum   2:subtract  3:multiply  4:divide    5:factorial   6:pow");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+}
